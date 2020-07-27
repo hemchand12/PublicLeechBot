a46f5d28ba108b6e6d159d7ca1c7e0e328c2e311
artemiszuk <68847756+artemiszuk@users.noreply.github.com>
2020-07-27 15:38:32 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -54,49 +54,49 @@ if __name__ == "__main__" :
     #
     incoming_message_handler = MessageHandler(
         incoming_message_f,
-        filters=Filters.command(["leech@leecheriruru"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["leech@leecheriruru_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_message_handler)
     #
     incoming_youtube_dl_handler = MessageHandler(
         incoming_youtube_dl_f,
-        filters=Filters.command(["ytdl@leecheriruru"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["ytdl@leecheriruru_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(incoming_youtube_dl_handler)
     #
     status_message_handler = MessageHandler(
         status_message_f,
-        filters=Filters.command(["status@leecheriruru"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["status@leecheriruru_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(status_message_handler)
     #
     cancel_message_handler = MessageHandler(
         cancel_message_f,
-        filters=Filters.command(["cancel@leecheriruru"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["cancel@leecheriruru_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(cancel_message_handler)
     #
     exec_message_handler = MessageHandler(
         exec_message_f,
-        filters=Filters.command(["exec@leecheriruru"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["exec@leecheriruru_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(exec_message_handler)
     #
     rename_message_handler = MessageHandler(
         rename_message_f,
-        filters=Filters.command(["rename@leecheriruru"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["rename@leecheriruru_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(rename_message_handler)
     #
     upload_document_handler = MessageHandler(
         upload_document_f,
-        filters=Filters.command(["upload@leecheriruru"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["upload@leecheriruru_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(upload_document_handler)
 
     help_text_handler = MessageHandler(
         help_message_f,
-        filters=Filters.command(["help"]) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(["help@leecheriruru_bot"]) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(help_text_handler)
     #
